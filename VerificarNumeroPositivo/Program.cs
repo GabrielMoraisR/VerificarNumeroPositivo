dfc217f60a990f3f41cdc40bc00e818cc258a665
Gabriel de Morais <morais.g99@gmail.com>
2023-06-25 16:35:50 -0300

    Adicionar arquivos de projeto.

---
+++ VerificarNumeroPositivo/Program.cs
@@ -0,0 +1,17 @@
+﻿class VerificarNumeroPositivo
+{
+    static void Main()
+    {
+        Console.Write("Digite um número inteiro: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("O número é positivo.");
+        }
+        else
+        {
+            Console.WriteLine("O número não é positivo.");
+        }
+    }
+}
